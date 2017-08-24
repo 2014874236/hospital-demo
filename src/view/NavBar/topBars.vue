464cfe00bd39d45c85d3673ef10bf4b4b3511fe1
2014874236 <2066895536@qq.com>
2017-08-24 19:11:58 +0800

    '1.0'

---
+++ src/view/NavBar/topBars.vue
@@ -0,0 +1,139 @@
+<template>
+	<div class="top">
+		<!-- <el-row> -->
+		<div>
+			<!-- <el-button class="barsBtn">
+				<i class="fa fa-bars" aria-hidden="true"></i>
+			</el-button> -->
+			<span class="font-time">今天&nbsp;{{ time.month }}&nbsp;月{{ time.day }}&nbsp;日&nbsp;&nbsp;{{ todos[time.Day] }}&nbsp;&nbsp;{{ time.hours }}:{{ time.minutes }}</span>
+			<ul class="menu2">
+				<li>
+					<el-dropdown class="dropdown">
+					    <span class="el-dropdown-link dropdown-link">
+					   	 	<i class="Img">
+					   	 		<img src="../../assets/logo.png">
+					   	 	</i>
+					   	 	<span>{{per.name}}</span>
+					    </span>
+					    <el-dropdown-menu slot="dropdown">
+						    <el-dropdown-item>
+						    	<router-link to='/home/personal' style="text-decoration:none;color:#666;">个人信息</router-link>
+						    </el-dropdown-item>
+						    <el-dropdown-item>修改密码</el-dropdown-item> 
+					  	</el-dropdown-menu>
+					</el-dropdown>
+				</li>
+				<li>
+					<i class="fa fa-envelope-o" aria-hidden="true"></i>消息
+				</li>
+				<li>
+					<i class="fa fa-question-circle" aria-hidden="true"></i>帮助
+				</li>
+				<li class="exit">
+					<router-link to="/">
+						<i class="fa fa-sign-in" aria-hidden="true"></i>退出
+					</router-link>
+				</li>
+			</ul>
+			</div>
+		<!-- </el-row> -->
+	</div>
+</template>
+<style type="text/css" scoped>
+	.top{
+		line-height: 50px;
+		background: #fff;
+	}
+	/*三横导航按钮*/
+	/*.top .barsBtn{
+		border: 0;
+		border-radius: 0;
+		background: #f1f1f1;
+		width: 50px;
+		height: 50px;
+	}*/
+	
+	/*时间*/
+	.top .font-time{
+		margin-left: 15px;
+		float: left;
+	}
+	.top .font-time,.top .dropdown,.top .menu2 li{
+		color: #666;
+		font-size: 14px;
+	}
+	.top .menu2{
+		text-align: center;		
+		float: right;
+		margin-right:60px; 
+	}
+	.top .menu2 li{
+		display: inline-block;
+		list-style: none;
+		text-align: center;
+		margin-left: 30px;
+	}
+	.top .menu2 .exit a{
+		text-decoration: none;
+		color: #666;
+		font-size: 14px; 
+	}
+	.top .menu2 li i{
+		margin-right: 10px;
+	}
+	.top img{
+		width: 30px;
+		height: 30px;
+		vertical-align: middle;   /*使得图片垂直居中*/
+	}
+	.top .dropdown-link{
+		cursor: pointer;
+	}
+</style>
+<script>
+	import store from '../../store';
+  	export default {
+  		data() {
+	    	return {
+	    		per:store.state.per,
+	    		time:{
+	    			month:'',
+	    			day:'',
+	    			Day:'',
+	    			hours:'',
+	    			minutes:''
+	    		},
+	    		todos:["周日","周一","周二","周三","周四","周五","周六"]
+	    	}
+   		},
+   		mounted:function(){
+   			this.setTime();
+   		},
+
+   		methods:{
+   			// 获取当前的时间显示
+   			setTime:function(){
+   				let that = this;  
+   				console.log("年月日");
+   				setInterval(function(){
+   					var date=new Date(); //时间对象不能放在函数外面
+   					that.time.month=date.getMonth()+1;   //这里不能使用this
+	    			that.time.day=date.getDate();
+	    			that.time.Day=date.getDay();
+	    			that.time.hours=date.getHours();
+	    			if(date.getMinutes<10){  
+	    				that.time.minutes='0'+date.getMinutes();
+	    			}
+	    			else{
+	    				that.time.minutes=date.getMinutes();
+	    			}
+	    			
+   				},1000);
+   			}
+   		}
+  	}
+  	// var cla=document.getElementsByClassName("barsBtn");
+  	// cla.onclick=function(){
+  	// 	this.toggle("aa")
+  	// }
+</script>
---
+++ src/view/reservation/liuContent.vue
@@ -0,0 +1,52 @@
+<template >
+	<div class="liuContent">
+		<div class="content-right">
+			<div class="content-right-top">
+			    <el-row class="row">
+					<span class="p1-name">刘医生</span>
+					<el-radio-button>出诊</el-radio-button>
+					<el-time-picker is-range v-model="time" placeholder="选择时间范围">
+					</el-time-picker>	  							 
+				</el-row>	
+			</div>
+			<div class="content-right-bottom">
+				<img src="../../assets/40.png" class="bells" > 
+				<p>暂无预约信息</p>		
+			</div>
+		</div>
+	</div>	
+</template>
+
+<script>
+// import "../../assets/css/style1.css"
+	//npm i element-ui -S 等同于
+	//npm install element-ui --save
+	export default {
+	  name: 'home',
+	  data: function () {
+	    return {
+	    	time:''
+	    }
+	  }
+	}
+</script>
+
+<style>
+	.liuContent .content-right .content-right-top{
+		margin-top:1.25%;
+	}
+	.liuContent .content-right .content-right-top .row{
+		line-height:50px;
+	}
+	.liuContent .content-right .content-right-bottom{
+		padding-top: 2%;
+	}
+	.liuContent .content-right .content-right-bottom .bells{
+		margin-left: 40%;
+		margin-top: 16%;
+	}
+	.liuContent .content-right .content-right-bottom p{
+		margin-left: 45%;
+		opacity: 0.6;
+	}
+</style>
